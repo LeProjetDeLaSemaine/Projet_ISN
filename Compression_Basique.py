db27d50b6c4e42daaaa1c6690b20b660bc907ef4
Cl3ment <cl3mentdev@gmail.com>
2015-01-18 18:01:22 +0100

    Version impérative de la compression basique
    Premier commit de la version impérative de la compression basique

---
+++ Compression_Basique.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# COMPRESSION BASIQUE
+# Principe : On remplace les répétitions d'un caractère par le nombre de
+# répétitions, suivi du caractère lui-même : aaabbc => 3a2bc
+
+def isInt(s):
+	try: #Oui, j'utilise un try/except, honte à moi...
+		int(s)
+		return True
+	except ValueError:
+		return False
+
+def compresser(content):
+	cntnt = "" #cntnt est la version compressée de content (eh oui lololol)
+	precedent = content[0]
+	n = 1 #compteur de lettres
+	for c in content[1:]: #on boucle dans la chaîne à décompresser (sauf la 1ere lettre, qui est déjà dans precedent) 
+		if c == precedent: #si identique au caractère pécédent
+			n += 1 #on incrémente le compteur de lettres
+		else: #sinon
+			cntnt += str(n if n > 1 else "") + precedent #on ajoute à cntnt le nombre de répétitions suivi de la lettre
+			n = 1 #on reset le compteur
+			precedent = c #et on affecte le caractère actuel au précédent
+	cntnt += str(n if n > 1 else "") + precedent #il faut mettre le dernier caractère manuellement... A peaufiner 
+	return (cntnt if len(cntnt) < len(content) else content) #on retourne la chaîne compressée si elle est plus courte que l'originale
+
+def decompresser(content):
+	d = "" #d contiendra la chaîne décompressée
+	n = 0 #compteur
+	for c in content:#on boucle dans la chaîne à décompresser
+		if isInt(c): #si c'est un int
+			n = 10*n + int(c) #décalage pour formater le nombre
+		else: #sinon c'est un caractère, donc on le dégroupe
+			for j in range((n+1 if n<1 else n)): #on l'ajoute currentNum fois au résultat (ou une fois si currentNum = 0)
+				d += c #on ajoute n fois le caractère à c
+			n = 0 #on reset pour passer au caractère suivant
+	return d #on retourne d (you don't say)
+
+
+print(compresser("AAADDDDEEEEEDDDBBBBCDGKHEJGFDJFFFFFFFFFFFFFFFFFFFFF"))
+print(decompresser("3A4D5E3D4B1C1D1G1K1H1E1J1G1F1D1J21F"))
