f1643ebd8928910335100e05fa104baeaa667a83
Cl3ment <cl3mentdev@gmail.com>
2015-01-23 21:16:42 +0100

    Factorisation du code
    - Création du fichier Utilitaires.py, qui regroupe des fonctions utiles
    - Création de CompressionLZ77.py, qui reste à coder

---
+++ CompressionLZ77.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#A CODER
+
+from Utilitaires import isInt, erreur, compressionFichier
+
+#COMPRESSION/DECOMPRESSION
+def compression(content):
+	pass
+	
+def decompression(content):
+	pass
+	
+
--- Compression_Basique.py
+++ Compression_Basique.py
@@ -5,17 +5,9 @@
 # Principe : On remplace les répétitions d'un caractère par le nombre de
 # répétitions, suivi du caractère lui-même : aaabbc => 3a2bc
 
+from Utilitaires import isInt, erreur, compressionFichier
 
-def isInt(s):
-	try: #Oui, j'utilise un try/except, honte à moi...
-		int(s)
-		return True
-	except ValueError:
-		return False
-		
-def erreur(motif):
-	print("[Erreur] " + motif)
-
+#COMPRESSION/DECOMPRESSION
 def compresser(content):
 	cntnt = "" #cntnt est la version compressée de content (eh oui lololol)
 	precedent = content[0]
@@ -41,23 +33,6 @@ def decompresser(content):
 				d += c #on ajoute n fois le caractère à c
 			n = 0 #on reset pour passer au caractère suivant
 	return d #on retourne d (you don't say)
-	
-
-def compressionFichier(origine, destination, op):
-	#Compresse un fichier contenant du texte brut
-	if op not in ('c', 'd'):
-		erreur("Opération non reconnue.")
-		return
-	if ((origine[-4:] == ".txt") and (destination[-4:] == ".txt")) : #On vérifie que les fichiers passés comme arguments sont bien du type txt (en détectant l'extension .txt)
-		with open(origine, 'r') as file: #on ouvre le fichier devant être traité en mode lecture
-			content = file.read()
-			file.close() #On ferme le fichier
-		newStr = (compresser(content) if op == 'c' else decompresser(content))
-		with open(destinatation ,'w') as file: #On ouvre le fichier de destination en mode écriture
-			file.write(newStr) #On écrit le résultat dans le fichier
-			file.close() # On ferme le fichier
-	else:
-		erreur("Type de fichier incorrect.")
 
 
 #Tests de compression de chaine
@@ -65,5 +40,5 @@ print(compresser("AAADDDDEEEEEDDDBBBBCDGKHEJGFDJFFFFFFFFFFFFFFFFFFFFF"))
 print(decompresser("3A4D5E3D4B1C1D1G1K1H1E1J1G1F1D1J21F"))
 
 #Tests de compression de fichier
-compressionFichier("Fichiers\\CompressionBasique\\TexteInitial.txt", "Fichiers\\CompressionBasique\\TexteCompressé.txt", 'c')
-compressionFichier("Fichiers\\DécompressionBasique\\TexteCompressé.txt", "Fichiers\\DécompressionBasique\\TexteDecompressé.txt", 'd')
+#compressionFichier("Fichiers\\CompressionBasique\\TexteInitial.txt", "Fichiers\\CompressionBasique\\TexteCompressé.txt", 'c')
+#compressionFichier("Fichiers\\DécompressionBasique\\TexteCompressé.txt", "Fichiers\\DécompressionBasique\\TexteDecompressé.txt", 'd')
