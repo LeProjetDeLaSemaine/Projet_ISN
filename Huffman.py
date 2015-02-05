973245a244c9c24d231e96c6da9d5f35a20f320c
Cl3ment <cl3mentdev@gmail.com>
2015-02-05 23:01:25 +0100

    Bla-Bla

--- Huffman.py
+++ Huffman.py
@@ -12,11 +12,15 @@ def construireDict(content):
 			dico[c] = 1
 	return dico
 	
-def construireArbre(dico):
+def construireArbre(dico): #construit un arbre petit à petit en mode yolo (je suis fatigué ok ???)
 	tas = [(n, c) for n, c in dico.items()]
 	#implémenter la réorganisation du tas pour que ce soit vraiment un tas
-	#while len(tas) >= 2:
-	return tas
+	while len(tas) >= 2:
+		print("pas fait")
+		#n1, node1
+		#n2, node2
+		#Ajouter au tas un nouveau node qui a n1+n2 comme poids
+	return tas[1] #ou un pop de tas ?
 	
 def compression(content):
 	pass
