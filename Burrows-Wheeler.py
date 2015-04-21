26be1ee5731f1910bf2ee990194a741d045d7f67
LeProjetDeLaSemaine <leprojetdelasemaine@gmail.com>
2015-04-21 14:04:17 +0200

    Burrows-Wheeler (Suite et Fin)
    Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
    accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
    illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
    explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut
    odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione
    voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum,
    quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam
    eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat
    voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
    corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
    Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse,
    quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo
    voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio
    dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti
    atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati
    cupiditate non provident, similique sunt in culpa, qui officia deserunt
    mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
    facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis
    est eligendi optio, cumque nihil impedit, quo minus id, quod maxime
    placeat, facere possimus, omnis voluptas assumenda est, omnis dolor
    repellendus. Temporibus autem quibusdam et aut officiis debitis aut
    rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et
    molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente
    delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut
    perferendis doloribus asperiores repellat.

--- Burrows-Wheeler.py
+++ Burrows-Wheeler.py
@@ -2,27 +2,41 @@
 #-*- coding:utf-8 -*-
 
 def transformation(chaine):
-	#Transforme une chaine de caractère selon l'algorithme de la transformée de Burrows-Wheeler
-	#   chaine: chaine à transformer
-	#renvoie une chaine de caractères transformée
-	
-	l = len(chaine) #bonnes pratiques :-P
-	tableau = ["" for i in range(l)] #on crée une liste de chaînes de caractères (qui sont des listes) avec l entrées
-	
-	for i in range(l):
-		tableau[i] = chaine[i:] + chaine[:i] #avec un peu d'astuce, cette ligne permet de prendre les lettres après i et d'y ajouter celles avant i #fierté #ilovepython
-	tableau.sort() #On utilise ici une méthode de l'objet liste qu'il aurait été trop fastidieux de recoder
-	
+	"""Transforme une chaine de caractère selon l'algorithme de la transformée de Burrows-Wheeler"""
+
+	chaine = '^' + chaine #On met un délmiteur en début de chaine qui permettra de décoder par la suite
+	L = len(chaine)
+	tableau = ["" for i in range(L)] #on crée une liste de chaînes de caractères (qui sont des listes) avec L entrées
 	chaineTransformee = "" #texte transformé
-	index = int() #index du texte original dans tab
-	
-	for i in range(l):
-		chaineTransformee += tableau[i][l-1:] #la chaîne transformée est celle composée des derniers caractères de chaque chaîne du tableau => la dernière colonne 
-		if tableau[i] == chaine:
-			index = i
-	return str(index) + chaineTransformee #on retourne l'index de la chaîne originale dans le tableau trié suivi de la chaîne transformée
+
+	#CONSTRUCTION DE LA MATRICE	
+	for i in range(L):
+		tableau[i] = chaine[i:] + chaine[:i] #avec un peu d'astuce, cette ligne permet de prendre les lettres après i et d'y ajouter celles avant i
+	tableau.sort() #On trie la matrice avec la méthode sort() de l'objet liste
+
+	#TRANSFORMATION DE LA CHAINE	
+	for i in range(L):
+		chaineTransformee += tableau[i][L-1:] #la chaîne transformée est celle composée des derniers caractères de chaque chaîne du tableau => la dernière colonne 
+	return chaineTransformee #on retourne l'index de la chaîne originale dans le tableau trié suivi de la chaîne transformée
 
 def decodage(chaine):
-	pass #pas fait
-	
-print(transformation("MISSISSIPI RIVER"))
+        """décode une chaine transformée selon l'algorithme de Burrows-Wheeler"""
+
+        L = len(chaine)
+        tableau = ["" for i in range(L)]
+        
+	#RESTITUTION DE LA MATRICE
+        for j in range(0,L):
+                #A chaque étape, on concatène chaque chaine du tableau avec les caractères de la chaine puis on le trie par ordre alphabêtique
+                for i in range(0,L):
+                        tableau[i] = chaine[i] + tableau[i] 
+                tableau.sort()        
+        
+	#RECUPERATION DE LA CHAINE INITIALE
+        for i in tableau:
+                if(i[0] == '^'): 
+                        return i[1:] #On renvoie la chaine sans son premier caractère, qui correspond au délimiteur
+        return None #La chaine ne contient pas le délimiteur, elle n'est donc pas transformée, on renvoie None
+        
+print(transformation("BANANA"))
+print(decodage("BNN^AAA"))
